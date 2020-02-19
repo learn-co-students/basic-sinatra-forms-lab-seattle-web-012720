f08ade59051a4336a6670ca01b84d0675835e922
Clarion Heard <clarion.heard@gmail.com>
2020-02-18 17:58:52 -0800

    Done.

--- app.rb
+++ app.rb
@@ -2,5 +2,19 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
 
+    get '/newteam' do
+        erb :newteam
+    end
 
+    post '/newteam' do
+        @team_name = params[:name]
+        @coach = params[:coach]
+        @point = params[:pg]
+        @shooting = params[:sg]
+        @sf = params[:sf]
+        @pf = params[:pf]
+        @center = params[:c]
+
+        erb :team
+    end
 end
